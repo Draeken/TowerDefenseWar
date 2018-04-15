38a9ddbab15baebd69083391359ebb295bd581fd
PBM42 <pbm764927@gmail.com>
2018-04-15 18:30:54 +0200

    feat: player can spawn tower

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public GameObject towerPrefab;
+
+  private new Camera camera;
+  // Use this for initialization
+  void Start()
+  {
+    camera = GetComponent<Camera>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Ray rayCast = camera.ScreenPointToRay(Input.mousePosition);
+      var tower = Instantiate(towerPrefab, rayCast.GetPoint(49), Quaternion.identity);
+    }
+  }
+}
