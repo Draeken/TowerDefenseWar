4d276e30e95082543430ee981056566e11438497
PBM42 <pbm764927@gmail.com>
2018-04-17 08:41:27 +0200

    feat: add attacker controller with interval wave spawning

---
+++ Assets/Scripts/AttackerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerController : MonoBehaviour
+{
+  public GameObject mobPrefab;
+  public Vector3 startPoint;
+
+  // Use this for initialization
+  void Start()
+  {
+    InvokeRepeating("SendWave", 1.0f, 5.0f);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void SendWave()
+  {
+    Instantiate(mobPrefab, startPoint, Quaternion.identity);
+  }
+}
