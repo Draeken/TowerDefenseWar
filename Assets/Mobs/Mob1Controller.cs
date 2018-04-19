da15840bc80a31289027382dbf917b5b0d0e248d
PBM42 <pbm764927@gmail.com>
2018-04-19 08:43:33 +0200

    feat: mob head for target base

---
+++ Assets/Mobs/Mob1Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mob1Controller : MonoBehaviour
+{
+
+  private GameObject targetBase;
+  private NavMeshAgent agent;
+
+  // Use this for initialization
+  void Start()
+  {
+    targetBase = GameObject.Find("TargetBase");
+    agent = GetComponent<NavMeshAgent>();
+    agent.destination = targetBase.transform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
